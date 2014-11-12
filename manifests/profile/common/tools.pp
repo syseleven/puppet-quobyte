733f2cb0ab8b949ad768e458a9851e7ba696fea6
Maik Zumstrull <m.zumstrull@syseleven.de>
2014-11-12 13:18:57 +0100

    Add ability to add device types to helper script and rename

--- manifests/profile/common/tools.pp
+++ manifests/profile/common/tools.pp
@@ -2,9 +2,9 @@ class quobyte::profile::common::tools () {
 
   ensure_packages(['xfsprogs', 'sysfsutils', 'python-jsonrpclib'])
 
-  file { '/usr/local/bin/quobyte_device_tags':
+  file { '/usr/local/bin/quobyte_device_settings':
     mode   => '0555',
-    source => "puppet:///modules/$module_name/quobyte_device_tags",
+    source => "puppet:///modules/$module_name/quobyte_device_settings",
   }
 
 }
--- manifests/profile/disks/qtags.pp
+++ manifests/profile/disks/qtags.pp
@@ -4,13 +4,13 @@ define quobyte::profile::disks::qtags (
 
   if $rotational == '1' {
     exec { "add-hdd-tag-$name":
-      command => "/usr/local/bin/quobyte_device_tags -u $quobyte::profile::server::api_service -t hdd $name && touch $name/.tagged",
+      command => "/usr/local/bin/quobyte_device_settings -u $quobyte::profile::server::api_service --tags hdd $name && touch $name/.tagged",
       onlyif  => "/usr/bin/test -f $name/QUOBYTE_DEV_ID",
       creates => "$name/.tagged",
     }
   } else {
     exec { "add-ssd-tag-$name":
-      command => "/usr/local/bin/quobyte_device_tags -u $quobyte::profile::server::api_service -t ssd $name && touch $name/.tagged",
+      command => "/usr/local/bin/quobyte_device_settings -u $quobyte::profile::server::api_service --tags ssd $name && touch $name/.tagged",
       onlyif  => "/usr/bin/test -f $name/QUOBYTE_DEV_ID",
       creates => "$name/.tagged",
     }
