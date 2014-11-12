73b8bc88cbfd22b714adcf40a71588a76f5fa59e
Maik Zumstrull <m.zumstrull@syseleven.de>
2014-11-12 17:22:04 +0100

    Forgot a s/.tagged/.typed/

--- manifests/profile/disks/qtypes.pp
+++ manifests/profile/disks/qtypes.pp
@@ -5,7 +5,7 @@ define quobyte::profile::disks::qtypes (
   $device_types = join($types, ',')
 
   exec { "add-device-types-$name":
-    command => "/usr/local/bin/quobyte_device_settings -u $quobyte::profile::server::api_service --types $device_types $name && touch $name/.tagged",
+    command => "/usr/local/bin/quobyte_device_settings -u $quobyte::profile::server::api_service --types $device_types $name && touch $name/.typed",
     onlyif  => "/usr/bin/test -f $name/QUOBYTE_DEV_ID",
     creates => "$name/.typed",
   }
