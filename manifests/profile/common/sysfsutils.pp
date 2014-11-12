fa6ee22eab5cc84963c593a8291ac8009c5c918d
Maik Zumstrull <m.zumstrull@syseleven.de>
2014-11-12 11:35:19 +0100

    Extend the PATH for a call
    Ubuntu's service script is not happy without bin in addition to sbin.

--- manifests/profile/common/sysfsutils.pp
+++ manifests/profile/common/sysfsutils.pp
@@ -2,7 +2,7 @@ class quobyte::profile::common::sysfsutils () {
 
   exec { 'sysfsutils-restart':
     command     => "service sysfsutils restart",
-    path        => ['/sbin', '/usr/sbin'],
+    path        => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
     refreshonly => true,
   }
 
