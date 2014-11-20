e9aff7bf03473f08e49e088aea223e7098fd5bc0
Johannes Grassler <j.grassler@syseleven.de>
2014-11-20 17:16:27 +0100

    quobyte_dir: added support for configuration parameters enabled_networks and public_ip.

--- manifests/profile/server/directory.pp
+++ manifests/profile/server/directory.pp
@@ -1,5 +1,7 @@
 class quobyte::profile::server::directory (
   $run_service = true,
+  $enabled_networks = undef,
+  $public_ip = undef,
 ) {
 
   service { 'quobyte-dir':
@@ -7,4 +9,28 @@ class quobyte::profile::server::directory (
     enable => $run_service,
   }
 
+  # Sets the enabled_networks directive in /etc/quobyte/dir.cfg
+  # You may have to set this explicitely because autodetection sometimes picks
+  # the wrong network.
+
+  if $enabled_networks {
+    file_line {'quobyte-dir-enabled_networks':
+      path   => '/etc/quobyte/dir.cfg',
+      line   => "registry.enabled_networks = $enabled_networks",
+      notify => Service['quobyte-dir'],
+    }
+  }
+
+  # Sets the public_ip directive in /etc/quobyte/dir.cfg
+  # You may have to set this explicitely because autodetection sometimes picks
+  # the wrong interface's address.
+
+  if $public_ip {
+    file_line {'quobyte-dir-public_ip':
+      path   => '/etc/quobyte/dir.cfg',
+      line   => "public_ip = $public_ip",
+      notify => Service['quobyte-dir'],
+    }
+
+  }
 }
