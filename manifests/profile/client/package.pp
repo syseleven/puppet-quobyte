067e439c77b54dade730fb666e0df9db15ff74dd
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-03-23 16:48:09 +0100

    Explicitly require repo for package installation

--- manifests/profile/client/package.pp
+++ manifests/profile/client/package.pp
@@ -1,5 +1,7 @@
 class quobyte::profile::client::package {
 
+  require quobyte::profile::common::repo
+
   package { 'quobyte-client':
     ensure => latest,
   }
--- manifests/profile/server/package.pp
+++ manifests/profile/server/package.pp
@@ -2,6 +2,8 @@ class quobyte::profile::server::package (
   $ensure_version = '1.0.RC1.p19.2',
 ) {
 
+  require quobyte::profile::common::repo
+
   package { 'quobyte-server':
     ensure => $ensure_version,
   }
