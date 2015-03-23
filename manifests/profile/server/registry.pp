9dd03f781d4a9887e27d0a25e9d10e4394ea3e4a
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-03-23 13:35:05 +0100

    Remove enabled networks parameter
    This is no longer handled via the config file, it's a runtime parameter.

--- manifests/profile/server/registry.pp
+++ manifests/profile/server/registry.pp
@@ -1,6 +1,5 @@
 class quobyte::profile::server::registry (
   $run_service = true,
-  $enabled_networks = undef,
   $public_ip = undef,
   $rpc_port = false,
   $http_port = false,
@@ -9,18 +8,6 @@ class quobyte::profile::server::registry (
   service { 'quobyte-registry':
     ensure => $run_service,
     enable => $run_service,
-  }
-
-  # Sets the enabled_networks directive in /etc/quobyte/registry.cfg
-  # You may have to set this explicitely because autodetection sometimes picks
-  # the wrong network.
-
-  if $enabled_networks {
-    file_line {'quobyte-registry-enabled_networks':
-      path   => '/etc/quobyte/registry.cfg',
-      line   => "registry.enabled_networks = $enabled_networks",
-      notify => Service['quobyte-registry'],
-    }
   }
 
   # Sets the public_ip directive in service configuration file. You may have
