9cabdaac0b1b02260b5574ea76c0ebaf3af6da53
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-03-23 13:38:26 +0100

    Remove obsolete anchor pattern from profiles

--- manifests/profile/server.pp
+++ manifests/profile/server.pp
@@ -5,9 +5,7 @@ class quobyte::profile::server (
   require quobyte::profile::common::repo
   require quobyte::profile::common::java8
 
-  anchor { 'server::start': }->
   class { 'quobyte::profile::server::package': }->
-  class { 'quobyte::profile::server::config': }->
-  anchor { 'server::end': }
+  class { 'quobyte::profile::server::config': }
 
 }
