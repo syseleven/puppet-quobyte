80f067a302d11edb46aae72847ee2b30d1d88120
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-03-23 13:57:33 +0100

    Ensure Quobyte services are always managed

--- manifests/profile/server.pp
+++ manifests/profile/server.pp
@@ -1,5 +1,10 @@
 class quobyte::profile::server (
   $api_service = undef,
+  $run_registry = false,
+  $run_metadata = false,
+  $run_data = false,
+  $run_api = false,
+  $run_webconsole = false,
 ) {
 
   require quobyte::profile::common::repo
@@ -7,5 +12,30 @@ class quobyte::profile::server (
 
   class { 'quobyte::profile::server::package': }->
   class { 'quobyte::profile::server::config': }
+
+  class { 'quobyte::profile::server::registry':
+    run_service = $run_registry,
+    require => Class['quobyte::profile::server::package'],
+  }
+
+  class { 'quobyte::profile::server::metadata':
+    run_service = $run_metadata,
+    require => Class['quobyte::profile::server::package'],
+  }
+
+  class { 'quobyte::profile::server::data':
+    run_service = $run_data,
+    require => Class['quobyte::profile::server::package'],
+  }
+
+  class { 'quobyte::profile::server::api':
+    run_service = $run_api,
+    require => Class['quobyte::profile::server::package'],
+  }
+
+  class { 'quobyte::profile::server::webconsole':
+    run_service = $run_webconsole,
+    require => Class['quobyte::profile::server::package'],
+  }
 
 }
