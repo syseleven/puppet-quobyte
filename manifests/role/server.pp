a8ac5c41e4f79d7508aef5f6302a1a44c8b0fe27
Johannes Grassler <j.grassler@syseleven.de>
2015-05-29 14:32:03 +0200

    os-606 Temporarily disabled bootstrap profile.

--- manifests/role/server.pp
+++ manifests/role/server.pp
@@ -3,7 +3,7 @@ class quobyte::role::server {
 include quobyte::profile::client
 include quobyte::profile::disks
 include quobyte::profile::server
-include quobyte::profile::server::bootstrap
+#include quobyte::profile::server::bootstrap
 
 Class['quobyte::profile::disks'] -> Class['quobyte::profile::server']
 
