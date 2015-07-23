585464896aba3cb9550034b5f30c8c552d53a636
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-07-23 12:14:52 +0200

    os-656 Generate a Quobyte system config file

--- manifests/profile/server/config.pp
+++ manifests/profile/server/config.pp
@@ -1,5 +1,12 @@
 class quobyte::profile::server::config (
   $registry,
+  $smtp_host = 'localhost',
+  $smtp_port = 25,
+  $smtp_sender = 'quobyte@localhost',
+  $keystone_hostname = '',
+  $keystone_port = 35357,
+  $keystone_user_id = '',
+  $keystone_password = '',
 ) {
 
   file { '/etc/quobyte/host.cfg':
@@ -8,6 +15,14 @@ class quobyte::profile::server::config (
     group   => 'quobyte',
     mode    => '0444',
     content => "# Managed by Puppet\nregistry=${registry}\ndebug.level=6\n",
+  }
+
+  file { '/etc/quobyte/system.cfg':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'quobyte',
+    mode    => '0440',
+    content => template("${module_name}/system.cfg.erb"),
   }
 
 }
