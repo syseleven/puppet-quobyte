91463918b4da2a0a9bb3316630f3cd9994d5cadd
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-09-03 13:05:44 +0200

    Remove support for overriding the API endpoint
    Since we're deploying the load balancers after the Quobyte bootstrap,
    referencing them here is pointless - we must use 127.0.0.1.

--- manifests/profile/volumes.pp
+++ manifests/profile/volumes.pp
@@ -1,15 +1,12 @@
 # Creates Quobyte volumes and their configurations. It is best to only run this on one machine to avoid race conditions.
 class quobyte::profile::volumes (
   $volumes = {},
-  $api_service = hiera('quobyte::profile::server::api_service'),
   $cinder_stripe_width = 5,
   $glance_stripe_width = 1,
   $nova_stripe_width = 5,
   $cinder_cache_size = 50,
   $nova_cache_size = 50,
   ) {
-    $api_url = regsubst($api_service, 'admin:quobyte@', '')
-
     # Add an appropriately named file (see below) and an entry to this list for new volume configs.
     $volume_configs = ['BASE', 'cinder', 'nova', 'glance']
 
@@ -19,7 +16,6 @@ class quobyte::profile::volumes (
 
     quobyte_volumes{ $volumes_keys:
       volumes => $volumes,
-      api_url => $api_url,
       require => Quobyte_volume_configs[$volume_configs],
     }
 
@@ -38,7 +34,6 @@ class quobyte::profile::volumes (
 
     define quobyte_volumes(
       $volumes,
-      $api_url,
     ) {
       if $volumes['mode'] {
         $mode = $volumes[$name]['mode']
@@ -57,7 +52,6 @@ class quobyte::profile::volumes (
         group   => $volumes[$name]['group'],
         mode    => $mode,
         config  => $config,
-        api_url => $api_url,
       }
     }
   }
