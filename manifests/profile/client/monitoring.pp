bdfac2d44e3840d55edda9f0f0632da9549d1d43
Stefan Andres <s.andres@syseleven.de>
2015-09-21 15:43:41 +0200

    os-852 only alert von third occurrence

--- manifests/profile/client/monitoring.pp
+++ manifests/profile/client/monitoring.pp
@@ -11,8 +11,8 @@ class quobyte::profile::client::monitoring (
         source => "puppet:///modules/$module_name/monitoring/check_quobyte_client_versions",
       }->
       sensu::check{'quobyte_client_versions':
-        command     => '/usr/lib/nagios/plugins/check_quobyte_client_versions',
-        occurrences => 3,
+        command             => '/usr/lib/nagios/plugins/check_quobyte_client_versions',
+        alert_on_occurrence => 3,
       }
     }
     false:  { }
