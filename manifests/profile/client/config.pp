67165a6d74a39b88ed1e57ceb3930b6dfb542210
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-10-06 10:27:51 +0200

    Force user xattr enabled

--- manifests/profile/client/config.pp
+++ manifests/profile/client/config.pp
@@ -4,6 +4,14 @@ class quobyte::profile::client::config {
     ensure  => file,
     mode    => '0444',
     content => "# Managed by puppet\nuser_allow_other\n",
+    require => Package['quobyte-client'],
+  }
+
+  file_line { 'quobyte_user_xattr':
+    path    => '/etc/quobyte/client.cfg',
+    line    => 'fuse_option=user_xattr',
+    match   => '^fuse_option',
+    require => Package['quobyte-client'],
   }
 
 }
